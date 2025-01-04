287535c1e2a9362699bc44f5cc61d3d70948c5b2
devmishra888 <devmishra888@gmail.com>
2025-01-04 15:50:43 +0530

    created a submission

---
+++ script.js
@@ -0,0 +1,25 @@
+	function highlight() {
+     let boldWords = document.querySelectorAll('b');
+    
+   
+    boldWords.forEach(function(word) {
+        word.style.color = 'green';
+    });
+
+
+}
+
+
+function return_normal() {
+      let boldWords = document.querySelectorAll('b');
+    
+   
+    boldWords.forEach(function(word) {
+        word.style.color = 'black';
+    });
+
+    
+}
+
+
+
